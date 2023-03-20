535398fa4a2492c12fab465184c726253c6dc0f7
Liam O <lokanedeveloper@gmail.com>
2023-03-20 20:23:31 +1100

    going through week 1 work of the uni course. For a refresher

---
+++ funstuff.js
@@ -0,0 +1,7 @@
+function addTheValue() {
+  console.log("this is generating");
+  //   firstValue.innerHTML = parseInt(fvalue.innerHTML) + parseInt(secondValue);
+  document.getElementById("firstValue").innerHTML = "Hello World";
+}
+
+// document.getElementById("secondValue").innerHTML =
